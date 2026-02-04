a8713611dce61400001d7e6e867a14888c0b46e9
Pratyay <typrotypro220@gmail.com>
2026-02-04 13:24:47 +0530

    Fixed Mobile Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -102,17 +102,18 @@ const Navbar = () => {
                       label="Cart"
                       onClick={() => router.push("/cart")}
                     />
-                  </UserButton.MenuItems>
-                </UserButton>
-                <UserButton>
-                  <UserButton.MenuItems>
-                    <UserButton.Action
+                     <UserButton.Action
                       labelIcon={<PackageIcon size={16} />}
                       label="My Orders"
                       onClick={() => router.push("/orders")}
                     />
                   </UserButton.MenuItems>
                 </UserButton>
+                {/* <UserButton>
+                  <UserButton.MenuItems>
+                   
+                  </UserButton.MenuItems>
+                </UserButton> */}
               </div>
             ) : (
               <button
